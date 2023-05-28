15b06f78b262bf1c23235decec964fb1d61bf134
vamosakos <vamos.akos1337@gmail.com>
2023-05-28 04:56:46 +0200

    display datas on table & delete method

--- src/EvenTour/src/EvenTourFormA.java
+++ src/EvenTour/src/EvenTourFormA.java
@@ -1,12 +1,11 @@
 import com.toedter.calendar.JDateChooser;
+import net.proteanit.sql.DbUtils;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Date;
 
 
@@ -18,11 +17,9 @@ public class EvenTourFormA extends JDialog{
     private JTextField tfPrice;
     private JButton btnAdd;
     private JTable evenTourDashboardTable;
-    private JButton btnUpdate;
     private JButton btnDelete;
-    private JButton btnSearch;
-    private JTextField tfSearch;
     private JButton btnLogout;
+    private JTextField tfId;
 
 
     JDateChooser datechooser = new JDateChooser();
@@ -39,6 +36,7 @@ public class EvenTourFormA extends JDialog{
 
         datechooser.setDateFormatString("dd/MM/yyyy");
         jpDatePicker.add(datechooser);
+        tableLoad();
 
         btnAdd.addActionListener(new ActionListener() {
             @Override
@@ -54,24 +52,46 @@ public class EvenTourFormA extends JDialog{
             }
         });
 
+        btnDelete.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) { deleteTour(); }
+        });
+
         setVisible(true);
     }
 
 
+    PreparedStatement pst;
+    private void tableLoad() {
+        Tour tour = null;
+        final String DB_URL ="jdbc:mysql://localhost/eventour?serverTimezone=UTC";
+        final String USERNAME = "root";
+        final String PASSWORD = "";
 
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+
+            Statement stmt = conn.createStatement();
+
+            pst = conn.prepareStatement("SELECT * FROM tours");
+            ResultSet rs = pst.executeQuery();
+            evenTourDashboardTable.setModel(DbUtils.resultSetToTableModel(rs));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
     private void addTour() {
         String route = tfRoute.getText();
         Date date = datechooser.getDate();
         int length = Integer.parseInt(tfLength.getText());
         int price = Integer.parseInt(tfPrice.getText());
-        if (route.isEmpty() || date == null || length == 0 || price == 0) {
+        if (route.isEmpty() || date == null) {
             JOptionPane.showMessageDialog(this,
                     "Please enter all fields",
                     "Try again",
                     JOptionPane.ERROR_MESSAGE);
             return;
         }
-
         tour = addTourToDatabase(route, date, length, price);
         if (tour != null) {
             JOptionPane.showMessageDialog(this,
@@ -87,12 +107,34 @@ public class EvenTourFormA extends JDialog{
         }
     }
 
+    private void deleteTour() {
+
+        int id = Integer.parseInt(tfId.getText());
+
+        tour = deleteTourFromDatabase(id);
+        if (tour != null) {
+            JOptionPane.showMessageDialog(this,
+                    "Tour successfully deleted",
+                    "Success",
+                    JOptionPane.INFORMATION_MESSAGE);
+        }
+        else {
+            JOptionPane.showMessageDialog(this,
+                    "Failed to delete tour",
+                    "Try again",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+
+
+    }
     public Tour tour;
     private Tour addTourToDatabase(String route, Date date, int length, int price) {
         Tour tour = null;
         final String DB_URL ="jdbc:mysql://localhost/eventour?serverTimezone=UTC";
         final String USERNAME = "root";
         final String PASSWORD = "";
+
+
 
         try {
             Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
@@ -115,8 +157,10 @@ public class EvenTourFormA extends JDialog{
                 tour.price = price;
             }
 
+
             stmt.close();
             conn.close();
+            tableLoad();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -124,4 +168,38 @@ public class EvenTourFormA extends JDialog{
 
         return tour;
     }
+    private Tour deleteTourFromDatabase(int id) {
+        Tour tour = null;
+        final String DB_URL ="jdbc:mysql://localhost/eventour?serverTimezone=UTC";
+        final String USERNAME = "root";
+        final String PASSWORD = "";
+
+
+
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+
+            Statement stmt = conn.createStatement();
+            String sql = "DELETE FROM tours where id = ? ";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+
+            int addedRows = preparedStatement.executeUpdate();
+            if (addedRows > 0) {
+                tour = new Tour();
+                tour.id = id;
+            }
+
+
+            stmt.close();
+            conn.close();
+            tableLoad();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return tour;
+    }
+
 }
--- src/EvenTour/src/Tour.java
+++ src/EvenTour/src/Tour.java
@@ -2,6 +2,7 @@ import java.time.LocalDate;
 import java.util.Date;
 
 public class Tour {
+    public int id;
     public String route;
     public Date date;
     public int length;
