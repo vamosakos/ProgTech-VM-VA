cb98d3614de2cf3cfaf77fb9b758b5e99b84927f
vamosakos <vamos.akos1337@gmail.com>
2023-05-28 23:52:19 +0200

    modified main class

--- src/EvenTour/src/Main.java
+++ src/EvenTour/src/Main.java
@@ -1,27 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        /*
-        RegistrationForm regForm = new RegistrationForm(null);
-
-        User user = regForm.user;
-        if (user != null) {
-            System.out.println("Successful registration of: " + user.full_name);
-        }
-        else {
-            System.out.println("Registration canceled");
-        }
-        */
-
-
         LoginForm loginForm = new LoginForm(null);
-        User user = loginForm.user;
-
-        if (user != null) {
-            System.out.println("Successful Authentication of: " + user.full_name);
-            System.out.println("            Email: " + user.email);
-        }
-        else {
-            System.out.println("Authentication canceled");
-        }
     }
 }
